eecdd607dcae445a5f08b6ca6077af24ea924cf4
ruhan <robiuzzaman4@gmail.com>
2025-10-14 16:55:02 +0600

    feat: pattern related problem solved on phitron oj

--- phitron-oj/topic/c/Pattern_1.c
+++ phitron-oj/topic/c/Pattern_1.c
@@ -5,9 +5,9 @@ int main()
     int n;
     scanf("%d", &n);
 
-    for (int i = n; i >= 1; i--)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = i; j >= 1; j--)
+        for (int j = 1; j <= i; j++)
         {
             printf("%d ", j);
         }
@@ -17,5 +17,3 @@ int main()
 
     return 0;
 }
-
-#include <stdio.h>
