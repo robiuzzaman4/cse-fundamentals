89993168702d60d6433e0713fe7e6cf2ebd0333d
ruhan <robiuzzaman4@gmail.com>
2025-10-25 23:07:21 +0600

    fix: sheet-1 G problem time limit exceed issue fix

--- semester-1/group-contests/sheet-1/G_Summation_from_1_to_N.c
+++ semester-1/group-contests/sheet-1/G_Summation_from_1_to_N.c
@@ -5,12 +5,7 @@ int main()
     long long int n;
     scanf("%lld", &n);
 
-    long long int sum = 0;
-
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i;
-    }
+    long long int sum = n * (n + 1) / 2;
 
     printf("%lld", sum);
 
