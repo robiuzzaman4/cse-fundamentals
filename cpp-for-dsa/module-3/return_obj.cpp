1c4c635305ad464c6dec626f00f9f0b81e146e6c
ruhan <robiuzzaman4@gmail.com>
2025-11-20 01:24:26 +0600

    feat: cpp-for-dsa module-3.5 done

---
+++ cpp-for-dsa/module-3/return_obj.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+
+public:
+    int roll;
+    int cls;
+    int cgpa;
+    Student(int r, int c, double g)
+    {
+        roll = r;
+        cls = c;
+        cgpa = g;
+    }
+};
+
+Student fun()
+{
+    Student Ruhan(4, 10, 4.06);
+    return Ruhan;
+}
+
+int main()
+{
+    Student ruhan = fun();
+
+    cout << ruhan.roll << " " << ruhan.cls << " " << ruhan.cgpa;
+
+    return 0;
+}
