6822e6751b34c03d4a198d20008f417475ef0cd3
ruhan <robiuzzaman4@gmail.com>
2025-11-28 22:42:46 +0600

    feat: cpp-for-dsa module-5 complete

---
+++ semester-1/cpp-for-dsa/module-5/string_input_with_space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    cout << x << endl;
+    cout << s << endl;
+    return 0;
+}
---
+++ semester-1/cpp-for-dsa/module-5/string_iterators.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string t = "Taahia";
+
+    // for (string::iterator it = t.begin(); it < t.end(); it++)
+    // {
+    //     cout << *it << " ";
+    // }
+    for (auto it = t.begin(); it < t.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    // cout << *t.begin() << endl;
+    // cout << *(t.end() - 1) << endl;
+
+    return 0;
+}
