5f308c93e30b9546ccd09dc2a9d7edb4b3d99b45
ruhan <robiuzzaman4@gmail.com>
2026-01-22 20:27:22 +0600

    feat: s-2 m-15 done

---
+++ semester-2/basic-data-structures/module-15/in_order_traversal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// left -> root -> right
+void in_order_print(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    in_order_print(root->left);  // left
+    cout << root->value << " ";  // root
+    in_order_print(root->right); // right
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    in_order_print(root);
+
+    return 0;
+}
---
+++ semester-2/basic-data-structures/module-15/post_order_traversal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// left -> right -> root
+void post_order_print(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    post_order_print(root->left);  // left
+    post_order_print(root->right); // right
+    cout << root->value << " ";    // root
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    post_order_print(root);
+
+    return 0;
+}
